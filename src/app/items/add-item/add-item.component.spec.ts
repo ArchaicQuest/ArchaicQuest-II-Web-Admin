0c5757753e923f3ed6496b311500c1d35e10fa44
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2018-12-06 17:10:26 +0000

    tests!

--- src/app/items/add-item/add-item.component.spec.ts
+++ src/app/items/add-item/add-item.component.spec.ts
@@ -1,33 +1,47 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { APP_BASE_HREF } from "@angular/common";
+import { AddItemComponent } from "./add-item.component";
+import { SharedModule } from "../../shared/shared.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { itemRoutes } from "../item.routes";
+import { addItemReducer } from "../state/add-item.reducer";
+import { StoreModule } from "@ngrx/store";
+import { AddItemEffects } from "../state/add-item.effects";
+import { EffectsModule } from "@ngrx/effects";
+import {
+  MatSelectModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatCheckboxModule,
+  MatTableModule,
+  MatButtonModule,
+  MatPaginatorModule,
+  MatAutocompleteModule
+} from "@angular/material";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ViewItemsComponent } from "../view-items/view-items.component";
+import { AppRoutingModule } from "src/app/app-routing.module";
+import { FlagEnum } from "../interfaces/flags.enums";
 
-import { AddItemComponent } from './add-item.component';
-import { SharedModule } from '../../shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { itemRoutes } from '../item.routes';
-import { addItemReducer } from '../state/add-item.reducer';
-import { StoreModule } from '@ngrx/store';
-import { AddItemEffects } from '../state/add-item.effects';
-import { EffectsModule } from '@ngrx/effects';
-import { MatSelectModule, MatFormFieldModule, MatInputModule, MatCheckboxModule, MatTableModule, MatButtonModule, MatPaginatorModule } from '@angular/material';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-describe('AddItemComponent', () => {
+fdescribe("AddItemComponent", () => {
   let component: AddItemComponent;
   let fixture: ComponentFixture<AddItemComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddItemComponent ],
+      declarations: [AddItemComponent, ViewItemsComponent],
       imports: [
-        SharedModule,
-        HttpClientModule,
         BrowserAnimationsModule,
+        AppRoutingModule,
+        HttpClientModule,
+        SharedModule,
         ReactiveFormsModule,
-        EffectsModule.forRoot([]),
         StoreModule.forRoot({}),
-        StoreModule.forFeature('item', addItemReducer),
+        EffectsModule.forRoot([]),
+        RouterModule.forChild(itemRoutes),
+        StoreModule.forFeature("item", addItemReducer),
         EffectsModule.forFeature([AddItemEffects]),
         MatSelectModule,
         MatFormFieldModule,
@@ -35,10 +49,11 @@ describe('AddItemComponent', () => {
         MatCheckboxModule,
         MatButtonModule,
         MatTableModule,
-        MatPaginatorModule
-      ]
-    })
-    .compileComponents();
+        MatPaginatorModule,
+        MatAutocompleteModule
+      ],
+      providers: [{ provide: APP_BASE_HREF, useValue: "/" }]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -47,13 +62,59 @@ describe('AddItemComponent', () => {
     fixture.detectChanges();
   });
 
-  it(`should have as title 'ArchaicQuestII'`, async(() => {
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ArchaicQuestII');
-  }));
+  // it(`should have as title 'ArchaicQuestII'`, async(() => {
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('ArchaicQuestII');
+  // }));
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should have flag Anti Evil', () => {
+    const app = component;
+    app.selectedFlag = FlagEnum.Antievil;
+    const hasAntiEvilFlag = app.hasFlag(FlagEnum.Antievil);
+    expect(hasAntiEvilFlag).toEqual(true);
+  });
 
+  it('should have flag Glow and Hum', () => {
+    const app = component;
+    app.selectedFlag = FlagEnum.Glow;
+    const hasGlowFlag = app.hasFlag(FlagEnum.Glow);
+    expect(hasGlowFlag).toEqual(true);
+
+    app.selectedFlag = FlagEnum.Hum;
+    const hasHumFlag = app.hasFlag(FlagEnum.Hum);
+    expect(hasHumFlag).toEqual(true);
+  });
+
+  it('should toggle Armour Section', () => {
+    const app = component;
+
+    app.toggleItemSection(0);
+
+    expect(app.showArmourSection).toEqual(true);
+    expect(app.showBookSection).toEqual(false);
+    expect(app.showContainerSection).toEqual(false);
+    expect(app.showWeaponSection).toEqual(false);
+  });
+
+  it('should toggle Weapon Section', () => {
+    const app = component;
+
+    app.toggleItemSection(11);
+
+    expect(app.showArmourSection).toEqual(false);
+    expect(app.showBookSection).toEqual(false);
+    expect(app.showContainerSection).toEqual(false);
+    expect(app.showWeaponSection).toEqual(true);
+  });
+
+  it('should toggle Book Section', () => {
+    const app = component;
+
+    app.toggleItemSection(1);
+
+    expect(app.showArmourSection).toEqual(false);
+    expect(app.showBookSection).toEqual(true);
+    expect(app.showContainerSection).toEqual(false);
+    expect(app.showWeaponSection).toEqual(false);
+  });
 });
