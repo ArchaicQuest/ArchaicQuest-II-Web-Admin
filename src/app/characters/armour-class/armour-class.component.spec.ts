2f37a2d439be58111d187e01b7b09df77ec0f66b
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2019-04-10 00:30:15 +0100

    issue with form group / missing property ?

--- src/app/characters/armour-class/armour-class.component.spec.ts
+++ src/app/characters/armour-class/armour-class.component.spec.ts
@@ -14,7 +14,7 @@ import { APP_BASE_HREF } from '@angular/common';
 import { EffectsModule, Actions } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from 'src/app/app-routing.module';
@@ -27,47 +27,47 @@ import { ArmourClassComponent } from './armour-class.component';
 describe('Armour Rating Component', () => {
     let fixture: ComponentFixture<ArmourClassComponent>;
     const mockItemData: Item = {
-          id: 0,
-          name: 'key',
-          armourRating: {
-              armour: 5,
-              magic: 3
-          },
-          armourType: 1,
-          attackType: 0,
-          book: null,
-          condition: 1,
-          container: null,
-          containerItems: null,
-          damage: {
-              maximum: 2,
-              minimum: 1
-          },
-          damageType: 1,
-          decayTimer: 1,
-          description: {
-              exam: '',
-              look: '',
-              room: '',
-              smell: '',
-              taste: '',
-              touch: ''
-          },
-          forageRank: 1,
-          hidden: false,
-          infinite: false,
-          isHiddenInRoom: false,
-          itemFlag: null,
-          itemType: '1',
-          keywords: null,
-          knownByName: false,
-          level: '1',
-          questItem: false,
-          slot: 1,
-          stuck: false,
-          weaponType: 1,
-          weight: 0
-      };
+        id: 0,
+        name: 'key',
+        armourRating: {
+            armour: 5,
+            magic: 3
+        },
+        armourType: 1,
+        attackType: 0,
+        book: null,
+        condition: 1,
+        container: null,
+        containerItems: null,
+        damage: {
+            maximum: 2,
+            minimum: 1
+        },
+        damageType: 1,
+        decayTimer: 1,
+        description: {
+            exam: '',
+            look: '',
+            room: '',
+            smell: '',
+            taste: '',
+            touch: ''
+        },
+        forageRank: 1,
+        hidden: false,
+        infinite: false,
+        isHiddenInRoom: false,
+        itemFlag: null,
+        itemType: '1',
+        keywords: null,
+        knownByName: false,
+        level: '1',
+        questItem: false,
+        slot: 1,
+        stuck: false,
+        weaponType: 1,
+        weight: 0
+    };
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -91,7 +91,7 @@ describe('Armour Rating Component', () => {
             ],
             providers: [
                 { provide: APP_BASE_HREF, useValue: '/' },
-
+                FormBuilder
             ],
             schemas: [
                 CUSTOM_ELEMENTS_SCHEMA,
@@ -101,11 +101,33 @@ describe('Armour Rating Component', () => {
 
 
         fixture = TestBed.createComponent(ArmourClassComponent);
+        fixture.componentInstance.equipment = new FormBuilder().group({
+            armsEq: [''],
+            bodyEq: [''],
+            faceEq: [''],
+            feetEq: [''],
+            fingerEq: [''],
+            finger2Eq: [''],
+            floatingEq: [''],
+            handsEq: [''],
+            heldEq: [''],
+            legsEq: [''],
+            lightEq: [''],
+            neckEq: [''],
+            neck2Eq: [''],
+            shieldEq: [''],
+            torsoEq: [''],
+            waistEq: [''],
+            wristEq: [''],
+            wrist2Eq: [''],
+            headEq: [''],
+            sheathedEq: [''],
+            wieldEq: ['']
+        });
 
-        fixture.componentInstance.equipment = new FormGroup({
-          lightEq: null
-      });
-      fixture.detectChanges();
+        fixture.detectChanges();
+
+
     });
 
 
@@ -115,10 +137,13 @@ describe('Armour Rating Component', () => {
 
 
     it('should return armour rating', () => {
-      fixture.componentInstance['setArmourRating'](mockItemData);
 
-      expect( fixture.componentInstance.Defense).toBe(5);
-      expect( fixture.componentInstance.magicDefense).toBe(3);
+
+
+        fixture.componentInstance['setArmourRating'](mockItemData);
+
+        expect(fixture.componentInstance.Defense).toBe(5);
+        expect(fixture.componentInstance.magicDefense).toBe(3);
     });
 
     // this.equipment.get('lightEq').valueChanges.subscribe((value: Item) => {
@@ -127,19 +152,24 @@ describe('Armour Rating Component', () => {
 
 
     fit('should update AC when helmet is equiped', () => {
-      fixture.componentInstance.equipment.get('lightEq').setValue(mockItemData);
 
-      expect( fixture.componentInstance.Defense).toBe(5);
-      expect( fixture.componentInstance.magicDefense).toBe(3);
+
+        //  fixture.componentInstance.subscribeToEQChanges();
+        fixture.componentInstance.equipment.get('lightEq').setValue(mockItemData);
+
+        fixture.detectChanges();
+
+        expect(fixture.componentInstance.Defense).toBe(5);
+        expect(fixture.componentInstance.magicDefense).toBe(3);
     });
 
 
 
-it('should return armour rating', () => {
-      fixture.componentInstance['setArmourRating'](mockItemData);
+    it('should return armour rating', () => {
+        fixture.componentInstance['setArmourRating'](mockItemData);
 
-      expect( fixture.componentInstance.Defense).toBe(5);
-      expect( fixture.componentInstance.magicDefense).toBe(3);
+        expect(fixture.componentInstance.Defense).toBe(5);
+        expect(fixture.componentInstance.magicDefense).toBe(3);
     });
 
 
--- src/app/characters/armour-class/armour-class.component.ts
+++ src/app/characters/armour-class/armour-class.component.ts
@@ -1,105 +1,105 @@
 import {
-  Component,
-  OnInit,
-  OnDestroy,
-  Input
+    Component,
+    OnInit,
+    OnDestroy,
+    Input
 } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Item } from 'src/app/items/interfaces/item.interface';
 
 @Component({
-  selector: 'app-armour-class',
-  templateUrl: './armour-class.component.html'
+    selector: 'app-armour-class',
+    templateUrl: './armour-class.component.html'
 })
 export class ArmourClassComponent implements OnInit {
-  Defense = 0;
-  magicDefense = 0;
+    Defense = 0;
+    magicDefense = 0;
 
-  @Input() equipment: FormGroup;
+    @Input() equipment: FormGroup;
 
-  constructor() {}
+    constructor() { }
 
-  ngOnInit() {
-   this.subscribeToEQChanges();
+    ngOnInit() {
+        this.subscribeToEQChanges();
 
-  }
-
-  subscribeToEQChanges(): void {
-    this.equipment.get('lightEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('headEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('neck2Eq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('bodyEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('torsoEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('waistEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('legsEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('armsEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('wristEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('wristEq2').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('fingerEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('finger2Eq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('heldEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('shieldEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('wieldEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('sheathedEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-    this.equipment.get('floatingEq').valueChanges.subscribe((value: Item) => {
-      this.setArmourRating(value);
-    });
-
-  }
-
-  private setArmourRating(value: Item): void {
-    if (value != null) {
-      this.Defense += value.armourRating.armour;
-      this.magicDefense += value.armourRating.magic;
     }
 
-  }
+    subscribeToEQChanges(): void {
+        this.equipment.get('lightEq').valueChanges.subscribe((value: Item) => {
+            this.setArmourRating(value);
+        });
+
+        this.equipment.get('headEq').valueChanges.subscribe((value: Item) => {
+            this.setArmourRating(value);
+        });
+
+        // this.equipment.get('neck2Eq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('bodyEq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('torsoEq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('waistEq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('legsEq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('armsEq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('wristEq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('wristEq2').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('fingerEq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('finger2Eq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('heldEq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('shieldEq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('wieldEq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('sheathedEq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+        // this.equipment.get('floatingEq').valueChanges.subscribe((value: Item) => {
+        //   this.setArmourRating(value);
+        // });
+
+    }
+
+    private setArmourRating(value: Item): void {
+        if (value != null) {
+            this.Defense += value.armourRating.armour;
+            this.magicDefense += value.armourRating.magic;
+        }
+
+    }
 }
