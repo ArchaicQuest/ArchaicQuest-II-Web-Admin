3cf9d186638fef657840b9c6a4b2393f549c52b7
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2019-07-23 00:03:29 +0100

    fixed environment

--- src/app/World/area/view-areas/view-areas.service.ts
+++ src/app/World/area/view-areas/view-areas.service.ts
@@ -3,12 +3,13 @@ import { tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Area } from '../interface/area.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ViewAreasService {
-    private host = 'http://localhost:57814/api/';
+    private host = environment.hostAPI;
     private getAreasUrl = `${this.host}World/Area`;
 
     constructor(private http: HttpClient) { }
