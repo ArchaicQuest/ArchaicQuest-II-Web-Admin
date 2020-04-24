26e1b5e99b56152cf6500ffad2562a6501b5a04c
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2020-04-24 10:29:47 +0100

    dark theme wip

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 
 @Component({
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+    displayedColumns: string[] = ['name', 'race', 'class', 'level', 'actions'];
+    dataSource: MatTableDataSource<any>;
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
 
     constructor() {
 
     }
 
     ngOnInit() {
+        this.dataSource = new MatTableDataSource([{
+            name: "liam",
+            class: "Mage",
+            race: "Elf",
+            level: 30
+        }, {
+            name: "Kencori",
+            class: "Fighter",
+            race: "Gnome",
+            level: 30
+        }
+            , {
+            name: "Malleus",
+            class: "Cleric",
+            race: "Dwarf",
+            level: 30
+        }, {
+            name: "Apsalr",
+            class: "Rogue",
+            race: "Human",
+            level: 30
+        }])
 
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
     }
 
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { dashboardRoutes } from './dashboard.routes';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material';
+import { MatIconModule, MatTableModule, MatPaginatorModule } from '@angular/material';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { LineChartComponent } from './line-chart/line-chart.component';
 import { DashboardComponent } from './dashboard.component';
@@ -16,7 +16,9 @@ import { DashboardComponent } from './dashboard.component';
         RouterModule.forChild(dashboardRoutes),
         MatMenuModule,
         MatIconModule,
-        NgxChartsModule
+        NgxChartsModule,
+        MatTableModule,
+        MatPaginatorModule,
 
     ],
     providers: [
