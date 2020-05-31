eff53d7c2c80496565a62d1a20332dfcdbc71ed8
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2020-05-31 17:44:06 +0100

    view items data list updated

--- src/app/items/view-items/view-items.component.ts
+++ src/app/items/view-items/view-items.component.ts
@@ -1,41 +1,32 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
 import { Item } from '../interfaces/item.interface';
 import { ViewItemsService } from './view-items.service';
+import { DataListComponent } from 'src/app/shared/components/data-list/data-list.component';
 
 @Component({
     templateUrl: './view-items.component.html',
     styleUrls: ['./view-items.component.scss']
 })
-export class ViewItemsComponent implements OnInit {
+export class ViewItemsComponent extends DataListComponent implements OnInit {
     items: Item[] = [];
     displayedColumns: string[] = ['id', 'name', 'itemType', 'slot', 'weaponType', 'attackType', 'armourType', 'questItem', 'actions'];
-    dataSource: MatTableDataSource<Item>;
-
-    @ViewChild(MatPaginator) paginator: MatPaginator;
-    @ViewChild(MatSort) sort: MatSort;
 
     constructor(private viewItemsService: ViewItemsService) {
-
+        super();
     }
 
     ngOnInit() {
 
         this.viewItemsService.getItemTypes().subscribe((items) => {
-            this.items = items;
-            console.log(items)
-            this.dataSource = new MatTableDataSource(this.items);
-            this.dataSource.paginator = this.paginator;
-            this.dataSource.sort = this.sort;
-        })
+            console.log(items);
+            this.data = items;
+            this.filteredata = this.data;
+        });
     }
 
     applyFilter(filterValue: string) {
-        this.dataSource.filter = filterValue.trim().toLowerCase();
-
-        if (this.dataSource.paginator) {
-            this.dataSource.paginator.firstPage();
-        }
+        const result = this.data.filter(x => x.name.toLowerCase().includes(filterValue));
+        this.filteredata = result;
     }
 
 }
