34f3f46088ef8747975fef50119e72aab541725d
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2020-06-14 00:44:07 +0100

    angular 9 upgrade WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
 
 @NgModule({
     imports: [RouterModule.forRoot(routes, {
-        scrollPositionRestoration: 'top'
+        scrollPositionRestoration: 'enabled'
     })],
     exports: [RouterModule]
 })
